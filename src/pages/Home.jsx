8b8f85e945d8d458a411d519fe1aff7340e1ea1e
Sven Kaack <sven.kaack@gmx.de>
2025-05-11 22:17:55 +0200

    updated App.jsx and pages folder

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+    return (
+        <>
+            <ul>
+                <li>
+                    {/* use array method */}
+                    <p>cardEntry placeholder</p>
+                </li>
+            </ul>
+        </>
+    );
+};
+
+export default Home;
