e03fc4d935ce287b17effab5bbfc16ade7204147
Sven Kaack <sven.kaack@gmx.de>
2025-05-14 17:59:44 +0200

    added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Form from "./components/Form";
 import OpenEntryCard from "./components/OpenEntryCard";
+import Footer from "./components/Footer";
 
 function App() {
   const [entries, setEntries] = useState(
     JSON.parse(localStorage.getItem("entries")) || []
-    
   );
   return (
     <>
       <Navbar />
       <Home entries={entries} setEntries={setEntries} />
       <Form entries={entries} setEntries={setEntries} />
+      <Footer />
     </>
   );
 }
