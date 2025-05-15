c102686a4534593419670477b53d298b4e73ff41
Ankita Malani <ankitamalani2021@gmail.com>
2025-05-15 15:44:02 +0200

    modify date alert

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -44,8 +44,8 @@ const Form = ({ entries, setEntries }) => {
     const updateEntry = [...entries, newEntry];
 
     const existindDate = entries.map((entry) => entry.date);
-    if (existindDate.include(formData.date)) {
-      alert("This date already has an entry, Please select another Date");
+    if (existindDate.includes(formData.date)) {
+      alert("This date already has an entry. Please select another date :)");
       return;
     }
 
